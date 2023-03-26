583061d0310c43bc731bc9391562c047103aad8a
Icastroo <ignacio9781@gmail.com>
2023-03-26 18:47:11 -0300

    Cambios en components, App y el index

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const ListadoPacientes = () => {
   return (
-    <h2>Listado Pacientes</h2>
+    <div className='w-1/2 lg:w-3/5'>
+      <h2 className='font-black text-3xl text-center'>Listado Pacientes</h2>
+      <p className='text-xl mt-5 mb-10 text-center'>
+        Administra tus {''}
+        <span className='text-indigo-600 font-bold'>Pacientes y Citas</span>
+      </p>
+
+      <div className='m-3 bg-white shadow-md px-5 py-10 rounded-xl'>
+        <p>
+          nombre: {''}
+          <span>Hook</span>
+        </p>
+      </div>
+    </div>
   )
 }
 
